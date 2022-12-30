0ae17ae5ea65615b7622ef2315128fb94524abaa
BuildTools <unconfigured@null.spigotmc.org>
2022-12-30 01:05:56 -0500

    Converted header to canvas to improve performance

--- scripts/header.js
+++ scripts/header.js
@@ -1,110 +1,196 @@
-const gridContainer = $("#gridContainer");
-
-// If the window is 750px wide or less, cell size = 50px, otheriwse  100
-const cellSize = $(window).width() <= 750 ? 50 : 100;
-$("#gridContainer").css("--cell-size", cellSize + "px");
-
-var colCount = Math.ceil($(window).width() / cellSize);
-var rowCount = Math.ceil($(window).height() / cellSize);
-
-var cellCount = colCount * rowCount;
-
-var animatedIntro = false;
-
-gridContainer.css({
-	"--colCount": Math.floor(colCount),
-	"--rowCount": Math.floor(rowCount),
-});
-
-var cells = [];
-
-createCells(cellCount);
-
-// Adds a new row to the grid
-function createCells(cellCount) {
-	cells = [];
-	for (var i = 0; i < cellCount; i++) {
-		var cell = $("<div class='cell'></div>");
-		if (animatedIntro == false) {
-			cell.css({
-				"--animation-delay": i * 0.02 + "s",
-			});
-		} else {
-			cell.css({
-				"--animation-delay": 0 + "s",
-			});
-			cell.addClass("dontAnimateIntro");
-			console.log("HERE");
-		}
-		gridContainer.append(cell);
-		cells.push(cell);
-	}
-	animatedIntro = true;
-}
-
-$(window).on("scroll", function () {
-	// If scrolled past 100vh, animatedIntro = true;
-	if ($(window).scrollTop() > $(window).height()) {
-		animatedIntro = true;
-	} else {
-		animatedIntro = false;
-	}
-});
-
-// on resize recalculate everything
-$(window).on("resize", function () {
-	gridContainer.empty();
-	colCount = Math.ceil($(window).width() / cellSize);
-	rowCount = Math.ceil($(window).height() / cellSize);
-	cellCount = colCount * rowCount;
-	gridContainer.css({
-		"--colCount": Math.floor(colCount),
-		"--rowCount": Math.floor(rowCount),
-	});
-	createCells(cellCount);
-});
-
-// every second picks a random cell and adds the class "animating" to it and then removes it 5 seconds later
-var currRow = 0;
-const maxDeviance = 1;
-// Picks a number between 0 and colCount
-var currColNumber = Math.floor(Math.random() * colCount);
-setTimeout(function () {
-	setInterval(function () {
-		// var randomCell = cells[Math.floor(Math.random() * cells.length)];
-		// Picks a random cell from the current row and adds the class "animating" to it and then removes it 5 seconds later
-		// console.log(Math.floor(Math.random() * colCount + currRow * colCount));
-		var upUntilCurrRow = currRow * colCount;
-		// Picks a number between -maxDeviance and maxDeviance
-		var randomDeviance =
-			Math.floor(Math.random() * (maxDeviance * 2 + 1)) - maxDeviance;
-		// Adds the deviance to the current col number
-		var randomColNumber = currColNumber + randomDeviance;
-		// If the random col number is less than 0, set it to 0
-		if (randomColNumber < 0) randomColNumber = 0;
-		// If the random col number is greater than the col count, set it to the col count
-		if (randomColNumber > colCount - 1) randomColNumber = colCount;
-		// Set randomCell to the cell at the random col number
-		var randomCell = cells[upUntilCurrRow + randomColNumber];
-
-		try {
-			randomCell.addClass("animating");
-		} catch (e) {
-			console.log("ERROR");
-		}
-		setTimeout(function () {
-			randomCell.removeClass("animating");
-		}, 5000);
-		currRow++;
-		if (currRow >= rowCount) {
-			currRow = 0;
-			currColNumber = Math.floor(Math.random() * colCount);
-		}
-	}, 50);
-}, 5000);
-
 var nameLetters = $("#name span");
 // For every letter in the name, add an index css variable to it
 for (var i = 0; i < nameLetters.length; i++) {
 	nameLetters[i].style.setProperty("--index", i);
 }
+
+var mouse = {
+	x: 0,
+	y: 0,
+};
+
+class Cell {
+	opacity = 0;
+	introAnimationDelay = 0;
+
+	currentlyAnimating = false;
+	mouseOver = false;
+	additionalInset = 0;
+
+	constructor(x, y, introAnimationDelay, skipIntro) {
+		this.x = x;
+		this.y = y;
+		this.introAnimationDelay = introAnimationDelay;
+		// Runs introanimation after a delay
+		if (!skipIntro) {
+			setTimeout(() => {
+				this.introAnimation(1);
+			}, this.introAnimationDelay);
+		} else {
+			this.opacity = 1;
+		}
+	}
+
+	draw() {
+		gridCTX.fillStyle = "rgba(25, 25, 25, " + this.opacity + ")";
+		// Draws cell taking into account the cell gap
+		gridCTX.fillRect(
+			this.x + (cellGap + this.additionalInset) / 2,
+			this.y + (cellGap + this.additionalInset) / 2,
+			cellSize - (cellGap + this.additionalInset),
+			cellSize - (cellGap + this.additionalInset)
+		);
+	}
+
+	update() {
+		this.draw();
+		// If the mouse is over the cell, set the opacity to 0.5
+		if (
+			mouse.x > this.x &&
+			mouse.x < this.x + cellSize &&
+			mouse.y > this.y &&
+			mouse.y < this.y + cellSize
+		) {
+			this.additionalInset = 2;
+			this.mouseOver = true;
+		}
+
+		if (this.mouseOver) {
+			if (
+				mouse.x < this.x ||
+				mouse.x > this.x + cellSize ||
+				mouse.y < this.y ||
+				mouse.y > this.y + cellSize
+			) {
+				this.additionalInset = 0;
+				this.mouseOver = false;
+			}
+		}
+	}
+
+	introAnimation(duration) {
+		if (this.currentlyAnimating) return;
+		this.currentlyAnimating = true;
+		duration ? duration : (duration = 0.5);
+		this.opacity = 0;
+		// Animate the opacity of the cell using no libraries
+		var opacityInterval = setInterval(() => {
+			this.opacity += 0.01;
+			if (this.opacity >= 1) {
+				this.opacity = 1;
+				this.currentlyAnimating = false;
+				clearInterval(opacityInterval);
+			}
+		}, (duration * 1000) / 100);
+	}
+
+	flash(duration, delay) {
+		if (this.currentlyAnimating) return;
+		this.currentlyAnimating = true;
+		duration = duration ? duration : 0.5;
+		delay = delay ? delay : 0;
+		var darkening;
+		setTimeout(() => {
+			this.opacity = 1;
+			darkening = true;
+			// Animate the opacity of the cell using no libraries
+			var opacityInterval = setInterval(() => {
+				if (this.opacity <= 0) {
+					darkening = false;
+				}
+				if (darkening) {
+					this.opacity -= 0.01;
+				} else {
+					this.opacity += 0.01;
+				}
+				if (this.opacity >= 1) {
+					this.opacity = 1;
+					this.currentlyAnimating = false;
+					clearInterval(opacityInterval);
+				}
+			}, duration);
+		}, delay);
+	}
+}
+
+// CANVAS STUFF
+var gridCanvas = $("#gridCanvas");
+// Make the canvas the size of the window
+gridCanvas.attr("width", $(window).width());
+gridCanvas.attr("height", $(window).height());
+$(window).on("resize", function () {
+	gridCanvas.attr("width", $(window).width());
+	gridCanvas.attr("height", $(window).height());
+	colCount = Math.ceil($(window).width() / cellSize);
+	rowCount = Math.ceil($(window).height() / cellSize);
+	createCells(colCount, rowCount, true);
+});
+
+const cellSize = $(window).width() <= 750 ? 100 : 125;
+
+var cellGap = 2;
+
+var colCount = Math.ceil($(window).width() / cellSize);
+var rowCount = Math.ceil($(window).height() / cellSize);
+
+var gridCTX = gridCanvas[0].getContext("2d");
+
+var cells = [];
+
+function createCells(colCount, rowCount, skipIntro) {
+	cells = [];
+	var cellCount = 0;
+	for (var i = 0; i < rowCount; i++) {
+		cells.push([]);
+		for (var j = 0; j < colCount; j++) {
+			cells[i].push(
+				new Cell(j * cellSize, i * cellSize, cellCount * 10, skipIntro)
+			);
+			cellCount++;
+		}
+	}
+}
+
+function animateRaindrop() {
+	var rainDrop = [];
+	// Picks a random cell in the first row
+	var randomCell = Math.floor(Math.random() * colCount);
+	rainDrop.push(cells[0][randomCell]);
+	for (var i = 1; i < rowCount; i++) {
+		// Pick a random number between -1 and 1
+		var randomNum = Math.floor(Math.random() * 3) - 1;
+		// Add the random number to the current cell's x position
+		randomCell += randomNum;
+		// If the cell is out of bounds, set it to the edge of the grid
+		if (randomCell < 0) randomCell = 0;
+		if (randomCell >= colCount) randomCell = colCount - 1;
+		// Add the cell to the raindrop array
+		rainDrop.push(cells[i][randomCell]);
+	}
+	for (var i = 0; i < rainDrop.length; i++) {
+		rainDrop[i].flash(10, i * 100);
+	}
+}
+
+createCells(colCount, rowCount);
+
+// Run animateRaindrop every 1 seconds after 5 seconds
+setTimeout(function () {
+	setInterval(animateRaindrop, 1500);
+}, 5000);
+
+drawFrame();
+
+function drawFrame() {
+	gridCTX.clearRect(0, 0, gridCanvas.width(), gridCanvas.height());
+	for (var i = 0; i < cells.length; i++) {
+		for (var j = 0; j < cells[i].length; j++) cells[i][j].update();
+	}
+	requestAnimationFrame(drawFrame);
+}
+
+$("body").on("mousemove", function (e) {
+	var rect = gridCanvas[0].getBoundingClientRect();
+	mouse.x = e.clientX - rect.left;
+	mouse.y = e.clientY - rect.top;
+});
